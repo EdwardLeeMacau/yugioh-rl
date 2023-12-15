8aa459ad52bf78725de3183cfafc551031194811
EdwardLeeMacau <b05901119@ntu.edu.tw>
2023-12-15 16:46:42 +0800

    Update env_config.py

--- env_config.py
+++ env_config.py
@@ -7,6 +7,28 @@ ENV_CONFIG = {
         'player1': { 'lifepoints': 8000 },
         'player2': { 'lifepoints': 8000 }
     },
-    'reward_type': 'step count reward'
-    # reward type should be 'win/loss', 'LP', or 'step count reward'
+
+    # Remove reward shaping, only use win/loss reward
+    # 'reward_kwargs': {
+    #     'type': 'win/loss',
+    # },
+
+    # Experiment range: [0.05, 0.1, 0.2]
+    # 'reward_kwargs': {
+    #     'type': 'LP',
+    #     'weight': 0.05
+    # },
+
+    # Experiment range: [0.5, 1.0, 2.0]
+    # 'reward_kwargs': {
+    #     'type': 'LP_linear_step',
+    #     'weight': 0.1
+    # },
+
+    # Experiment range: ?
+    'reward_kwargs': {
+        'type': 'LP_exp_step',
+        'temperature': 0.1,
+        'weight': 0.1
+    },
 }
